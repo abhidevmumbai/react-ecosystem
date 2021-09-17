9a5b46dd62f46402d938f8eb4b0572a0d19149b8
abhidevmumbai <abhidevmumbai@gmail.com>
2021-09-17 15:48:24 -0400

    Added TodoListItem component

---
+++ src/todos/TodoListItem.js
@@ -0,0 +1,13 @@
+const TodoListItem = ({ todo }) => {
+  return (
+    <div className="todo-item-container">
+      <h3>{todo.text}</h3>
+      <div className="button-container">
+        <button className="completed-button">Mark as Completed</button>
+        <button className="remove-button">Remove</button>
+      </div>
+    </div>
+  );
+};
+
+export default TodoListItem;
